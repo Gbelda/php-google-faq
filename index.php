39dcfce1d0eee898446528dc126ce8ae625599e3
gbelda <gbelda56@gmail.com>
2021-12-03 15:42:14 +0100

    create logo container element

--- index.php
+++ index.php
@@ -115,4 +115,26 @@ $qa = [
     
 ];
 
-var_dump($qa);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google - FAQ</title>
+    <link rel="stylesheet" href="./style.php">
+</head>
+<body>
+    <header>
+        <div class="logo_container">
+            <div class="logo">
+                <img src="https://loghi-famosi.com/wp-content/uploads/2020/09/Google-Logo.png" alt="" style="width: 70px;">
+            </div>
+            <h2>Privacy & Terms</h2>
+        </div>
+    </header>
+    <!-- ./HEADER -->
+</body>
+</html>
---
+++ style.php
@@ -0,0 +1,10 @@
+<?php
+
+header("Content-type: text/css");
+
+?>
+
+.logo_container{
+    display: flex;
+    align-items: center;
+}
